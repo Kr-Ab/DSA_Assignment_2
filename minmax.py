2e6d4c8aa7fb2d36cd137eb8f4391c0181194c40
Prateek Ram <prateek3098@gmail.com>
2022-06-10 21:18:11 +0530

    Comments added

--- minmax.py
+++ minmax.py
@@ -4,16 +4,22 @@ from msilib.schema import Class
 class MinMax:
     data = []
 
+    #initialising the array 
     def __init__(self, sample):
         self.data = sample.copy()
 
+    
+
+    #function to get the minimum and maximum value and their respective indexes from the array
     def getminmax(self, start, end, min, max):
         min_ndx = max_ndx = 0
+        #if only one element exists
         if start == end:
             min = max = self.data[start]
             min_ndx = max_ndx = start
         else:
             mid = start + ((end - start) // 2)
+            #recursion by dividing the array  
             first_half = self.getminmax(start, mid, min, max)
             second_half = self.getminmax(mid + 1, end, min, max)
 
@@ -31,8 +37,10 @@ class MinMax:
                 max = second_half[1]
                 max_ndx = second_half[3]
 
+        #returning minimum value , maximum value , minimum value index , maximum value index
         return [min, max, min_ndx, max_ndx]
 
+    #function to write the output on the file
     def getoutput(self, result):
         min = result[0]
         max = result[1]
@@ -52,25 +60,28 @@ class MinMax:
             return "decreasing " + str(min)
 
 
+#main with input error handling
 if __name__ == '__main__':
     try:
+        #opening the output file
         f = open("outputPS1.txt", "w")
-
+        #opening the input file
         with open("inputPS1.txt", 'r') as input:
             for line in input:
                 try: 
                     sample = list(map(int, line.split()))
-                except:
+                except: #Inputs apart from integer array
                     f.write("Invalid Input \n")
                     continue
-                if len(set(sample))==1:
+                if len(set(sample))==1: # single value input
                     f.write("Only one number found. Cannot distinguish between distributions \n")
                     continue
-                if len(sample)==0:
-                    f.write("Invalid Input \n")
+                if len(sample)==0:# no Value entered
+                    f.write("Empty Input Array \n")
                     continue
                 minmax = MinMax(sample)
 
+                #writing the output to the file
                 f.write(minmax.getoutput(minmax.getminmax(
                     0, len(sample)-1, float('inf'), float('-inf'))))
                 f.write("\n")
